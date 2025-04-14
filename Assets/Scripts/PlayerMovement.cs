bc57e3d2fde8c2d6520df86c54365bc445a952a2
FoppleRopple <josh4gamer@outlook.com>
2025-04-14 13:45:34 +1200

    Player object detects whether it is hitting the ground
    Player can no longer jump when it's not touching the ground

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,26 +1,53 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Initialize variables
     private Rigidbody2D rb;
-    [SerializeField] private float speedMult = 3;
-    [SerializeField] private float jumpHeight = 5;
+    [SerializeField] private float speedMult;
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private LayerMask groundLayer;
+    
+    private bool jumpReady = true;
+    private BoxCollider2D boxCaster;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Set components
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        boxCaster = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        
+        // Movement and jumping
         rb.linearVelocityX = Input.GetAxis("Horizontal") * speedMult;
-        if (Input.GetKey(KeyCode.Space))
+        
+        if (Input.GetKey(KeyCode.Space) && jumpReady == true)
         {
-            rb.linearVelocityY = jumpHeight;
+            Jump();
 
+        } else if (jumpReady == false)
+        {
+            jumpReady = OnGround();
+            Debug.Log("Off ground");
         }
+    }
+
+    // Functions
+    private void Jump()
+    {
+        jumpReady = false;
+        rb.linearVelocityY = jumpHeight;
+    }
+
+    private bool OnGround()
+    {
+        // Dunno if I can recycle this code to detect something else than the ground just yet
+        RaycastHit2D onGround = Physics2D.BoxCast(boxCaster.bounds.center, boxCaster.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return onGround.collider != null;
     }
 }
