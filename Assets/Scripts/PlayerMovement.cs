6e0e2f190b672bcda8d4375d9d5dd7d8b1c16b11
FoppleRopple <josh4gamer@outlook.com>
2025-04-14 12:14:45 +1200

    First iteration of player movement
    Able to move player with arrow keys and space bar. Player continually flies while space bar is held.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    [SerializeField] private float speedMult = 3;
+    [SerializeField] private float jumpHeight = 5;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.linearVelocityX = Input.GetAxis("Horizontal") * speedMult;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.linearVelocityY = jumpHeight;
+
+        }
     }
 }
